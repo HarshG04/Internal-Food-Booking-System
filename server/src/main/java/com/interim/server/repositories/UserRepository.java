86dba27dcee8b3bd167717ca44ebd4a206fdc01c
Goyal <2494393@cognizant.com>
2026-05-18 11:59:28 +0530

    Created User Repository

---
+++ server/src/main/java/com/interim/server/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.interim.server.repositories;
+
+import com.interim.server.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
