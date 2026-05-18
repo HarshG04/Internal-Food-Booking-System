5fbcd0f903bd2c8b80f6d5db1b6b7408c0d07c9d
D <2494594@cognizant.com>
2026-05-18 14:58:30 +0530

    Created Feedback Model

--- server/src/main/java/com/interim/server/models/Feedback.java
+++ server/src/main/java/com/interim/server/models/Feedback.java
@@ -1,5 +1,30 @@
 package com.interim.server.models;
 
-class Feedback {
-    
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "feedbacks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Feedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "order_item_id", nullable = false, unique = true)
+    private OrderItem orderItem;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    private String review;
+
+    @Column(name = "reviewed_at")
+    private LocalDateTime reviewedAt;
 }
